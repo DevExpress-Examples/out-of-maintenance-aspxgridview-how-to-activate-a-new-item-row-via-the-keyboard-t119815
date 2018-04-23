296a6e615a01d83e2e26f4d874a2f791a54c7b8e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Init(object sender, EventArgs e) {
+
+    }
+
+    protected void ASPxGridView1_CustomJSProperties(object sender, ASPxGridViewClientJSPropertiesEventArgs e) {
+        e.Properties["cpLastVisibleRowIndex"] = (ASPxGridView1.PageIndex == -1) ? ASPxGridView1.VisibleRowCount : 
+            Math.Min(ASPxGridView1.SettingsPager.PageSize * (ASPxGridView1.PageIndex + 1), ASPxGridView1.VisibleRowCount) - 1;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxGridView - How to activate a new item row via the keyboard
+
+
+This example illustrates how to switch the grid to the mode of adding a new row when pressing the Tab key while the grid bottom row is focused.<br /><br /><strong>See Also:</strong><br /><a href="https://www.devexpress.com/Support/Center/p/E2003">Using keyboard for ASPxGridView pages navigation</a> <br /><a href="https://www.devexpress.com/Support/Center/p/E4473">ASPxGridView- How to implement extended keyboard functionality</a>
+
+<br/>
+
+
